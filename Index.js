826f5824e4e0b6ec30140493e8b5e8261f452dba
coderxAyushman <149178164+coderxAyushman@users.noreply.github.com>
2023-11-10 09:04:32 +0530

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,67 +1,51 @@
-let randomNumber = Math.random();
-let computerMove = null;
-function Rock(){
 
-	if (randomNumber >= 0 && randomNumber < 1 / 3){
-		computerMove = 'Rock';
-	}
-	else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-		computerMove = 'Paper';
-	}
-	else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-		computerMove = 'Scissors';
-	}
-     
-     let result1 = document.getElementById('result')
+function playGame(playerMove){
+	let computerMove = pickcomputerMove();
 
+	 let result1 = document.getElementById('result');
      
-     if (computerMove === 'Rock') {
-     	result1 = 'It is a Tie'
-     }
-     else if (computerMove === 'Paper') {
-     	result1 = 'You lose'
-     } 
-      else if (computerMove === 'Scissors') {
-     	result1 = 'You win'
+     if (playerMove === 'Rock') {
+     	if (computerMove === 'Rock') {result1 = 'It is a Tie'}
+     	else if (computerMove === 'Paper') {result1 = 'You loose'}
+        else if (computerMove === 'Scissors') {result1 = 'You win'}
      }
 
-     result.textContent = `you picked Rock. Computer picked ${computerMove}. ${result1}` 
-}
 
-
-
-function Paper(){
-	
-	if (randomNumber >= 0 && randomNumber < 1 / 3){
-		computerMove = 'Rock';
-	}
-	else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-		computerMove = 'Paper';
-	}
-	else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-		computerMove = 'Scissors';
-	}
-     
-     let result1 = document.getElementById('result')
-
-     
-     if (computerMove === 'Paper') {
-     	result1 = 'It is a Tie'
-     }
-     else if (computerMove === 'Scissors') {
-     	result1 = 'You lose'
-     } 
-      else if (computerMove === 'Rock') {
-     	result1 = 'You win'
+    else if (playerMove === 'Paper') {
+     	if (computerMove === 'Paper') {result1 = 'It is a Tie'}
+     	else if (computerMove === 'Scissors') {result1 = 'You loose'}
+        else if (computerMove === 'Rock') {result1 = 'You win'}
      }
 
-     result.textContent = `you picked Paper. Computer picked ${computerMove}. ${result1}` 
+
+    else if (playerMove === 'Scissors') {
+     	if (computerMove === 'Scissors') {result1 = 'It is a Tie'}
+     	else if (computerMove === 'Rock') {result1 = 'You loose'}
+        else if (computerMove === 'Paper') {result1 = 'You win'}
+     }
+
+    const scores = {
+	  wins: 0,
+	  looses: 0,
+	  ties: 0,
+     }
+     
+    if(result1 === 'You win') {scores.wins++;}
+    else if (result1 === 'You loose') {scores.looses++;}
+    else if (result1 === 'It is a Tie') {scores.ties++;}
+     var score = document.getElementById('score');
+     result.textContent = `you picked ${playerMove}. Computer picked ${computerMove}. ${result1}.`
+      score.textContent = `Wins: ${scores.wins}, Losses: ${scores.looses}, Ties: ${scores.ties}.`                          
 }
 
 
 
 
-function Scissors(){
+
+function pickcomputerMove(){
+	const randomNumber = Math.random();
+    let computerMove = '';
+
 
 	if (randomNumber >= 0 && randomNumber < 1 / 3){
 		computerMove = 'Rock';
@@ -72,19 +56,6 @@ function Scissors(){
 	else if (randomNumber >= 2 / 3 && randomNumber < 1) {
 		computerMove = 'Scissors';
 	}
-     
-     let result1 = document.getElementById('result')
 
-     
-     if (computerMove === 'Rock') {
-     	result1 = 'You lose'
-     }
-     else if (computerMove === 'Paper') {
-     	result1 = 'You win'
-     } 
-      else if (computerMove === 'Scissors') {
-     	result1 = 'It is a Tie'
-     }
-
-     result.textContent = `you picked Scissors. Computer picked ${computerMove}. ${result1}` 
+	return computerMove;
 }
